71de91f1f14a70c14b581c4bcd8584058ac0731a
Roberto Reyes <robertostanleyreyes@gmail.com>
2025-02-20 16:49:04 -0600

    setting up flagsmith

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -2,9 +2,19 @@ import { render, screen } from "@testing-library/react";
 import Home from "../src/app/page";
 import "@testing-library/jest-dom";
 
+jest.mock("../src/app/utils/flagsmith.ts", () => {
+  return {
+    getEnvironmentFlags: jest.fn(() => {
+      return Promise.resolve({
+        isFeatureEnabled: () => true,
+      });
+    }),
+  };
+});
+
 describe("Home", () => {
-  it("renders a heading", () => {
-    render(<Home />);
+  it("renders a heading", async () => {
+    render(await Home());
 
     const docH = screen.getByRole("heading", {
       name: "Hello World",
