e7ca998278d7dc5d3bd7614e1e8c0330fbf60acc
Roberto-Stanley <32695636+Roberto-Stanley@users.noreply.github.com>
2025-02-20 20:51:15 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const flags = await flagsmith.getEnvironmentFlags();
   return (
     <main className="main">
-      <div>
+<div>
         <h2>Hello World, I'm making this changes to test my workflows</h2>
       </div>
       <Link href="/about">About</Link>
